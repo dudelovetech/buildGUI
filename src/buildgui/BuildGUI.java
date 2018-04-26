3756581f4eeb70abf1e1544604b67f10a621a8f9
dudelovetech <ds.sun.lau@gmail.com>
2018-04-25 22:39:17 -0700

    more files

--- src/buildgui/BuildGUI.java
+++ src/buildgui/BuildGUI.java
@@ -16,6 +16,7 @@ public class BuildGUI {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        new MouseEvent().setVisible(true);
     }
     
 }
